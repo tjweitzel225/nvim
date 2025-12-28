76f0e3f7a759fcc582308e8f483af2cc3f8a8690
tyler <tylerweitzel225@gmail.com>
2025-12-28 18:26:29 -0500

    fixed mini ai

--- lua/plugins/mini_ai.lua
+++ lua/plugins/mini_ai.lua
@@ -1,8 +1,92 @@
+local whichkey_ai = function(opts)
+    local objects = {
+        { ' ', desc = 'whitespace' },
+        { '"', desc = '" string' },
+        { "'", desc = "' string" },
+        { '(', desc = '() block' },
+        { ')', desc = '() block with ws' },
+        { '<', desc = '<> block' },
+        { '>', desc = '<> block with ws' },
+        { '?', desc = 'user prompt' },
+        { 'U', desc = 'use/call without dot' },
+        { '[', desc = '[] block' },
+        { ']', desc = '[] block with ws' },
+        { '_', desc = 'underscore' },
+        { '`', desc = '` string' },
+        { 'a', desc = 'argument' },
+        { 'b', desc = ')]} block' },
+        { 'c', desc = 'class' },
+        { 'd', desc = 'digit(s)' },
+        { 'e', desc = 'CamelCase / snake_case' },
+        { 'f', desc = 'function' },
+        { 'i', desc = 'indent' },
+        { 'o', desc = 'block, conditional, loop' },
+        { 'q', desc = 'quote `"\'' },
+        { 't', desc = 'tag' },
+        { 'u', desc = 'use/call' },
+        { '{', desc = '{} block' },
+        { '}', desc = '{} with ws' },
+    }
+
+    ---@type wk.Spec[]
+    local ret = { mode = { 'o', 'x' } }
+    ---@type table<string, string>
+    local mappings = vim.tbl_extend('force', {}, {
+        around = 'a',
+        inside = 'i',
+        around_next = 'an',
+        inside_next = 'in',
+        around_last = 'al',
+        inside_last = 'il',
+    }, opts.mappings or {})
+    mappings.goto_left = nil
+    mappings.goto_right = nil
+
+    for name, prefix in pairs(mappings) do
+        name = name:gsub('^around_', ''):gsub('^inside_', '')
+        ret[#ret + 1] = { prefix, group = name }
+        for _, obj in ipairs(objects) do
+            local desc = obj.desc
+            if prefix:sub(1, 1) == 'i' then
+                desc = desc:gsub(' with ws', '')
+            end
+            ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+        end
+    end
+    require('which-key').add(ret, { notify = false })
+end
 return {
-    'nvim-mini/mini.ai',
+    'echasnovski/mini.ai',
+    lazy = false,
+    keys = {
+        { 'a', mode = { 'x', 'o' } },
+        { 'i', mode = { 'x', 'o' } },
+        { 'g', mode = { 'x', 'o' } },
+    },
+    dependencies = {
+        {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            init = function()
+                -- no need to load the plugin, since we only need its queries
+                require('lazy.core.loader').disable_rtp_plugin('nvim-treesitter-textobjects')
+            end,
+        },
+    },
     opts = function()
         local ai = require('mini.ai')
         return {
+            mappings = {
+                -- Main textobject prefixes
+                around = 'a',
+                inside = 'i',
+                around_next = 'an',
+                inside_next = 'in',
+                around_last = 'al',
+                inside_last = 'il',
+                -- Move cursor to corresponding edge of `a` textobject
+                goto_left = 'g[',
+                goto_right = 'g]',
+            },
             n_lines = 500,
             custom_textobjects = {
                 o = ai.gen_spec.treesitter { -- code block
@@ -26,5 +110,11 @@ return {
                 U = ai.gen_spec.function_call { name_pattern = '[%w_]' }, -- without dot in function name
             },
         }
+    end,
+    config = function(_, opts)
+        require('mini.ai').setup(opts)
+        vim.schedule(function()
+            whichkey_ai(opts)
+        end)
     end,
 }
